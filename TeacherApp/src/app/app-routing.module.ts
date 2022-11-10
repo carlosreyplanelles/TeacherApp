f9ff943efeab86211128db68c32cf8f716648a2d
Francisco Sánchez <sanchezgarridof@gmail.com>
2022-11-10 19:13:29 +0100

    create routes students

--- TeacherApp/src/app/app-routing.module.ts
+++ TeacherApp/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StudentListComponent } from './components/student-list/student-list.component';
+import { StudentViewComponent } from './components/student-view/student-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", pathMatch: "full", redirectTo: "home" },
+  { path: "students", component: StudentListComponent },
+  { path: "student/:idStudent", component: StudentViewComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
