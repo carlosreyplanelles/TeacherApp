a46e729a66cac4cd05be4e73a31168491c41beb9
alberto-mm <alberto.martin.montero@gmail.com>
2022-11-10 12:41:06 +0100

    Acciones de getAll y getById
    Estudiantes

--- APITeachers/routes/api.js
+++ APITeachers/routes/api.js
@@ -1,8 +1,14 @@
+<<<<<<< HEAD
 var express = require('express');
 var router = express.Router();
 
 /* Using the admin.js file in the api folder. */
 router.use('/admin', require('./api/admin.js'));
+=======
+const router = require('express').Router();
+
+router.use('/students', require('./api/students'));
+>>>>>>> e90ee12 (Acciones de getAll y getById)
 
 router.use('/students', require('./api/students'));
 
