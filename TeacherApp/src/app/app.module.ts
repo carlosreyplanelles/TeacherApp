462fbd50c335fa11edac954d2ebb295b25fc0768
Francisco Sánchez <sanchezgarridof@gmail.com>
2022-11-10 18:53:10 +0100

    Generate components, service and interfaces

--- TeacherApp/src/app/app.module.ts
+++ TeacherApp/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule} from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentListComponent } from './components/student-list/student-list.component';
+import { StudentCardComponent } from './components/student-card/student-card.component';
+import { StudentViewComponent } from './components/student-view/student-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentListComponent,
+    StudentCardComponent,
+    StudentViewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ TeacherApp/src/app/components/student-card/student-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-student-card',
+  templateUrl: './student-card.component.html',
+  styleUrls: ['./student-card.component.css']
+})
+export class StudentCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
