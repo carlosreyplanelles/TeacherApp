8f23d38b9529c4fd209b499a1247fcab158b31ed
alberto-mm <alberto.martin.montero@gmail.com>
2022-11-11 19:28:12 +0100

    Refactorización del merge

--- APITeachers/helpers/validators.js
+++ APITeachers/helpers/validators.js
@@ -1,6 +1,7 @@
 const { validationResult } = require('express-validator');
 const { getAdminById } = require("../models/admin.model");
 const { getTeacherById } = require("../models/teacher.model");
+const Student = require('../models/student.model');
 
 const newAdmin = {
     name: {
@@ -41,7 +42,7 @@ const newAdmin = {
     },
     role_id: {
         exists: {
-            errorMessage: "The password field is required",
+            errorMessage: "The role_id field is required",
         },
         isInt: true,
         errorMessage: "The role field must be a number"
@@ -87,67 +88,6 @@ const updateAdmin = {
     }
 
 }
-
-/**
- * It checks if there are errors in the validation, if there are, it returns a 400 status code and the
- * errors
- * @param req - The request object.
- * @param res - The response object.
- * @param next - A function that is used to pass control to the next middleware function.
- * @returns a 400 status code and the errors.
- */
-const checkError = (req, res, next) => {
-    /* Checking if there are errors in the validation, if there are, it returns a 400 status code and the
-    errors. */
-    const errors = validationResult(req);
-    /* Checking if there are errors in the validation, if there are, it returns a 400 status code and the
-    errors. */
-    if (!errors.isEmpty()) {
-        return res.status(400).json(errors.mapped());
-    }
-    /* A function that is used to pass control to the next middleware function. */
-    next();
-};
-
-/**
- * It checks if the admin id exists in the database.
- * @param req - The request object.
- * @param res - The response object.
- * @param next - A function that is used to pass control to the next middleware function.
- */
-const checkAdmin = async (req, res, next) => {
-    const { adminid } = req.params;
-    /* Checking if the admin id exists. */
-    if (await getAdminById(adminid)) {
-        /* A function that is used to pass control to the next middleware function. */
-        next();
-    } else {
-        res.status(404).json({ Message: 'That admin user does not exist' });
-    }
-};
-
-/**
- * This function checks if the teacher id exists in the database. If it does, it passes control to the
- * next middleware function. If it doesn't, it returns a 404 status code with a message
- * @param req - The request object.
- * @param res - The response object.
- * @param next - A function that is used to pass control to the next middleware function.
- */
-const checkTeacher = async (req, res, next) => {
-    const { teacherid } = req.params;
-    /* Checking if the teacher id exists. */
-    if (await getTeacherById(teacherid)) {
-        /* A function that is used to pass control to the next middleware function. */
-        next();
-    } else {
-        res.status(404).json({ Message: 'That teacher user does not exist' });
-    }
-};
-
-module.exports = {
-    newAdmin, updateAdmin, checkError, checkAdmin, checkTeacher
-}
-const Student = require('../models/student.model');
 
 const newStudent = {
     name: {
@@ -234,12 +174,68 @@ const newStudent = {
 }
 
 /**
+ * It checks if there are errors in the validation, if there are, it returns a 400 status code and the
+ * errors
+ * @param req - The request object.
+ * @param res - The response object.
+ * @param next - A function that is used to pass control to the next middleware function.
+ * @returns a 400 status code and the errors.
+ */
+const checkError = (req, res, next) => {
+    /* Checking if there are errors in the validation, if there are, it returns a 400 status code and the
+    errors. */
+    const errors = validationResult(req);
+    /* Checking if there are errors in the validation, if there are, it returns a 400 status code and the
+    errors. */
+    if (!errors.isEmpty()) {
+        return res.status(400).json(errors.mapped());
+    }
+    /* A function that is used to pass control to the next middleware function. */
+    next();
+};
+
+/**
+ * It checks if the admin id exists in the database.
+ * @param req - The request object.
+ * @param res - The response object.
+ * @param next - A function that is used to pass control to the next middleware function.
+ */
+const checkAdmin = async (req, res, next) => {
+    const { adminid } = req.params;
+    /* Checking if the admin id exists. */
+    if (await getAdminById(adminid)) {
+        /* A function that is used to pass control to the next middleware function. */
+        next();
+    } else {
+        res.status(404).json({ Message: 'That admin user does not exist' });
+    }
+};
+
+/**
+ * This function checks if the teacher id exists in the database. If it does, it passes control to the
+ * next middleware function. If it doesn't, it returns a 404 status code with a message
+ * @param req - The request object.
+ * @param res - The response object.
+ * @param next - A function that is used to pass control to the next middleware function.
+ */
+const checkTeacher = async (req, res, next) => {
+    const { teacherid } = req.params;
+    /* Checking if the teacher id exists. */
+    if (await getTeacherById(teacherid)) {
+        /* A function that is used to pass control to the next middleware function. */
+        next();
+    } else {
+        res.status(404).json({ Message: 'That teacher user does not exist' });
+    }
+};
+
+/**
  * It checks if the student id exists in the database.
  * @param req - The request object.
  * @param res - The response object.
  * @param next - A function that is used to pass control to the next middleware function.
  */
-const checkStudent = async (req, res, next) => {
+ const checkStudent = async (req, res, next) => {
     const { studentId } = req.params;
     /* Checking if the student id exists. */
     if (await Student.getById(studentId)) {
@@ -251,5 +247,5 @@ const checkStudent = async (req, res, next) => {
 };
 
 module.exports = {
-    newStudent, checkError, checkStudent
+    newAdmin, updateAdmin, newStudent, checkError, checkAdmin, checkTeacher, checkStudent
 }
