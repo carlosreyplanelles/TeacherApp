c8f133fee015b731b37077a8ad433145e1ff76ae
Lauri <lauri.marquinez@gmail.com>
2022-11-12 13:12:47 +0100

    Home Map View

--- TeacherApp/src/app/app.module.ts
+++ TeacherApp/src/app/app.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AgmCoreModule } from '@agm/core';
 
+import { environment } from 'src/environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +13,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 import { AdminViewComponent } from './components/admin-view/admin-view.component';
 import { Error404Component } from './components/errors/error404/error404.component';
+
 
 @NgModule({
   declarations: [
@@ -29,7 +32,9 @@ import { Error404Component } from './components/errors/error404/error404.compone
     /* A module that is used to create forms. */
     ReactiveFormsModule,
     /* A module that is used to create forms. */
-    FormsModule
+    FormsModule,
+    /* Importing the google maps api key from the environment.ts file. */
+    AgmCoreModule.forRoot(environment.googleMaps)
   ],
   providers: [],
   bootstrap: [AppComponent]
