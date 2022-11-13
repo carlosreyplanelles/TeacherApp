686b168a8b9dedb693fca35cb870fd2e47da161a
Francisco Sánchez <sanchezgarridof@gmail.com>
2022-11-13 19:58:48 +0100

    generate components, services and interfaces

---
+++ TeacherApp/src/app/components/teacher-card/teacher-card.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-teacher-card',
+  templateUrl: './teacher-card.component.html',
+  styleUrls: ['./teacher-card.component.css']
+})
+export class TeacherCardComponent implements OnInit {
+
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ TeacherApp/src/app/components/teacher-list/teacher-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-teacher-list',
+  templateUrl: './teacher-list.component.html',
+  styleUrls: ['./teacher-list.component.css']
+})
+export class TeacherListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
