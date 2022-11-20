1db5792e2a4bcbc7da20c78c4693d47fc5c36c6a
Francisco Sánchez <sanchezgarridof@gmail.com>
2022-11-20 21:19:41 +0100

    create filter teacherBranch

--- TeacherApp/src/app/components/teacher-list/teacher-list.component.ts
+++ TeacherApp/src/app/components/teacher-list/teacher-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+
 // Delete
 import { TEACHERS } from 'mokdata.teachers.db';
 import { Teacher } from 'src/app/interfaces/teacher.interface';
@@ -11,12 +12,57 @@ import { TeachersService } from 'src/app/services/teachers.service';
 export class TeacherListComponent implements OnInit {
   
   arrTeachers: Teacher[] = [];
+  filterArrTeachers: Teacher[] = [];
+
+  priceFilters: any[] = [
+    { name: 'Name (A to Z)', value: 'name' },
+    { name: 'Price (low to high)', value: 'priceAsc' },
+    { name: 'Price (high to low)', value: 'priceDes' },
+  ];
+
+  branchFilters: any = [
+    { name: 'Ciencias', value: 'Ciencias', id: 'ciencias', isChecked: false },
+    {
+      name: 'Arte y Humanidades',
+      value: 'Arte y Humanidades',
+      id: 'arteHumanidades',
+      isChecked: false,
+    },
+    {
+      name: 'Ciencias de la Salud',
+      value: 'Ciencias de la Salud',
+      id: 'cienciasSalud',
+      isChecked: false,
+    },
+    {
+      name: 'Ingieniería y Arquitectura',
+      value: 'Ingieniería y Arquitectura',
+      id: 'ingieneriaArquitactura',
+      isChecked: false,
+    },
+  ];
+
+  selectedFilters = {
+    branches: [
+      'Ciencias',
+      'Arte y Humanidades',
+      'Ciencias de la Salud',
+      'Ingieniería y Arquitectura',
+    ],
+    priceMax: 1199,
+    priceMin: 1,
+    exp: 100,
+  };
+
   constructor(private teachersService: TeachersService) {
     // Delete
-    this.arrTeachers = TEACHERS;
   }
 
   ngOnInit(): void {
+    this.arrTeachers = TEACHERS;
+    this.filterArrTeachers = this.arrTeachers;
+    this.filteredTeachers();
+
     // try {
     //   let response = await this.teachersService.getAll(1);
     //   this.arrTeachers = response.data;
@@ -26,4 +72,43 @@ export class TeacherListComponent implements OnInit {
     //   console.log(err.error)
     // }
   }
+
+  filteredTeachers() {
+    this.filterArrTeachers = this.arrTeachers;
+
+    const { branches, priceMax, priceMin, exp } = this.selectedFilters;
+
+    this.filterArrTeachers = this.arrTeachers.filter(
+      ({ branch_title, price_hour, experience }) =>
+        branches.includes(branch_title) &&
+        price_hour < priceMax &&
+        price_hour > priceMin &&
+        experience <= exp
+    );
+  }
+
+  changesFilterBranches() {
+    this.selectedFilters.branches = [];
+
+    for (let i = 0; i < this.branchFilters.length; i++) {
+      if (this.branchFilters[i].isChecked) {
+        this.selectedFilters.branches.push(this.branchFilters[i].name);
+      }
+    }
+
+    if (this.selectedFilters.branches.length == 0) {
+      this.selectedFilters.branches = [
+        'Ciencias',
+        'Arte y Humanidades',
+        'Ciencias de la Salud',
+        'Ingieniería y Arquitectura',
+      ];
+    }
+
+    this.filteredTeachers();
+  }
+
+
+  chanPrice() {}
+  chanExperience() {}
 }
