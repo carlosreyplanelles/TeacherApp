d37e83575583bd79f8ed101c65b77f0a7f28cafa
Carlos Rey <carlosreyplanelles@gmail.com>
2022-12-02 18:28:28 +0000

    Modificaciones de ficheros

--- TeacherApp/src/app/components/class-booking/class-booking.component.ts
+++ TeacherApp/src/app/components/class-booking/class-booking.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { formatDate } from "@angular/common";
 
 @Component({
   selector: 'app-class-booking',
@@ -14,34 +15,33 @@ export class ClassBookingComponent implements OnInit {
   selectedSlot!:any;
   bookedClasses:any[]=[]
   classesByDate:any[]=[]
+  selectedClass:any = null
+
   constructor() { }
 
   ngOnInit(): void {
-    this.bookedClasses.push({date:'12/12/2022', hour:'10:00'})
+    this.bookedClasses.push({hour:'10:00'})
     this.startingHour = 9
     this.endingHour = 20
-    
-    for(let i=this.startingHour;i<=this.endingHour;i++){
-      let bookedClass = this.bookedClasses.find(c=>c.hour==i)
-      let slot = {
-        id:i,
-        hour: i+':00',
-        available: bookedClass == undefined
-      }
-      this.slots.push(slot)
-    }
+    this.createSlots()
   }
 
-  onSelect(event:any){
-    console.log("funciono")
+  onSelect(date:Date){
+
+    let format = 'yyyy-MM-dd'
+    let locale = 'en-US'
+
+    let formattedDate = formatDate(date, format ,locale)
+    this.createSlots(formattedDate)
+
+    this.selectedClass=null
   }
 
   selectTime(slot:any){
    this.selectedSlot = slot
   }
 
-  createSlots(date:any){
-    bookedClasses=classes.find(c=>c.date == date)
+  createSlots(date:String = ""){
     for(let i=this.startingHour;i<=this.endingHour;i++){
       let bookedClass = this.bookedClasses.find(c=>c.hour==i)
       let slot = {
