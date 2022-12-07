f729c6436f4f20e821f95b026f7d076e92e662e1
ihm972 <65023926+ihm972@users.noreply.github.com>
2022-12-07 14:44:49 +0000

    Actualización tabla perfil estudiante clases activas
    fecha y hora curso. 
    fecha de la reserva

--- APITeachers/routes/api/student-classes.js
+++ APITeachers/routes/api/student-classes.js
@@ -4,12 +4,14 @@ const Class = require('../../models/class.model');
 
 router.get('/:studentId', async (req, res) => {
     const { studentId } = req.params;
-
+    
     try {
-        const classInfo = await Class.getByStudent(studentId);
-        res.json(classInfo);
-    } catch (err) {
-        res.json({ error: err.message });
+
+        const classInfo = await Class.getByStudent(studentId);       
+        res.status(200).json(classInfo);
+    } 
+    catch (err) {
+        res.status(400).json({ error: err.message });
     }
 });
 
